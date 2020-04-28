339e802a975c069ce813917fda15a1d0f385a64f
rakeshkumar-verma <rakeshkumar.verma@hcl.com>
2020-04-28 11:42:08 +0530

    Button component : button model
    Button component  : button model

--- core/src/main/java/com/hcl/ecomm/core/models/ButtonModel.java
+++ core/src/main/java/com/hcl/ecomm/core/models/ButtonModel.java
@@ -5,20 +5,23 @@ import javax.inject.Inject;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
 
 @Model(adaptables = Resource.class)
 public class ButtonModel {
 
 	@Inject
+	@Default(values="btn-primary")
 	private String buttonType;
 
 	@Inject
+	@Default(values="false")
 	private String isButtonDisable;
 
 	@PostConstruct
 	protected void init() {
-		if (StringUtils.isNotEmpty(buttonType) && StringUtils.isNotEmpty(isButtonDisable)) {
+		if (StringUtils.isNotEmpty(isButtonDisable) && isButtonDisable.equals("true")) {
 			buttonType = buttonType.concat("__disabled");
 		}
 	}
