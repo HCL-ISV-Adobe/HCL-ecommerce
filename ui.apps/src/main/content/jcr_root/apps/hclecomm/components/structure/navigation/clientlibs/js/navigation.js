7825bce9462c3fcd4992d26dee6a54892c1be1f7
AyushHCL <ayushmishraims@gmail.com>
2020-05-12 20:50:58 +0530

    mobile navigationa styling and functionality done

--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/structure/navigation/clientlibs/js/navigation.js
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/structure/navigation/clientlibs/js/navigation.js
@@ -5,21 +5,38 @@ const toggleNavigation = (menuicon) => {
 
 const toggleMenuIcon = (selectedNavigationITEM, event) => {
     event.stopPropagation()
-$(selectedNavigationITEM).children(".cmp-navigation__icon-show").toggleClass("toggle-rotate-icon");
-    if($(selectedNavigationITEM).hasClass("toggle-sub-menu")) {
-$(selectedNavigationITEM).removeClass("toggle-sub-menu")
-}
-    else{
-$(selectedNavigationITEM).addClass("toggle-sub-menu")
-} 
+    $(selectedNavigationITEM).children(".cmp-navigation__icon-show").toggleClass("toggle-rotate-icon");
+    if ($(selectedNavigationITEM).hasClass("toggle-sub-menu")) {
+        $(selectedNavigationITEM).removeClass("toggle-sub-menu")
+    } else {
+        $(selectedNavigationITEM).addClass("toggle-sub-menu")
+    }
 }
 
 
 $(document).ready(manageNav);
-    $(window).on('resize',manageNav);
-    function manageNav(){
-        if($(window).width()<=1023){
-            $('li.cmp-navigation__item').children('ul.cmp-navigation__group').prev().css('pointer-events', 'none');
-    $('li.cmp-navigation__item').children('ul.cmp-navigation__group').siblings('span.cmp-navigation__icon').addClass('cmp-navigation__icon-show');
-        }
+$(window).on('resize', manageNav);
+function manageNav() {
+    if ($(window).width() <= 1023) {
+        $('li.cmp-navigation__item').children('ul.cmp-navigation__group').prev().css('pointer-events', 'none');
+        $('li.cmp-navigation__item').children('ul.cmp-navigation__group').siblings('span.cmp-navigation__icon').addClass('cmp-navigation__icon-show');
     }
+}
+
+$(document).ready(appendChildToNav);
+function appendChildToNav() {
+    const appendChildClass = 'cmp-navigation__item' + ' ' + 'cmp-navigation__item--level-1';
+    if ($(window).width() <= 1023) {
+        const parentObject = document.getElementsByClassName('cmp-navigation__item--level-0');
+        [...parentObject].forEach((parent, i) => {
+            if (parent.lastElementChild.classList.contains('cmp-navigation__group')) {
+                const copyParent = parent.children[1].cloneNode(true);
+                copyParent.removeAttribute('style');
+                let parentLi = document.createElement("li");
+                parentLi.className = 'cmp-navigation__item cmp-navigation__item--level-1'
+                parentLi.append(copyParent)
+                parent.lastElementChild.appendChild(parentLi);
+            }
+        });
+    }
+}
