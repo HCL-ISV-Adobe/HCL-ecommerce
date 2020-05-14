b943c825e510a2038a98bac30d15150f8287a34f
DeepaliAgrwl <deepalia@hcl.com>
2020-05-14 16:48:54 +0530

    latest css and js changes and added jquery in clientlib-base

--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/structure/navigation/clientlibs/js/navigation.js
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/structure/navigation/clientlibs/js/navigation.js
@@ -1,4 +1,3 @@
-
 const toggleNavigation = (menuicon) => {
     menuicon.classList.toggle("menu-icon");
     document.getElementById("cmp-navbar").classList.toggle("toggle-navigation");
@@ -7,11 +6,16 @@ const toggleNavigation = (menuicon) => {
 const toggleMenuIcon = (selectedNavigationITEM, event) => {
     event.stopPropagation()
     $(selectedNavigationITEM).children(".cmp-navigation__icon-show").toggleClass("toggle-rotate-icon");
-    if ($(selectedNavigationITEM).hasClass("toggle-sub-menu")) {
+      if ($(selectedNavigationITEM).hasClass("toggle-sub-menu")) {
         $(selectedNavigationITEM).removeClass("toggle-sub-menu")
     } else {
         $(selectedNavigationITEM).addClass("toggle-sub-menu")
     }
+
+
+    $(selectedNavigationITEM).children('ul').children('li').removeClass("toggle-sub-menu");
+    $(selectedNavigationITEM).children('ul').children('li').children('span').removeClass("toggle-rotate-icon");
+
 }
 
 
@@ -30,20 +34,20 @@ $(document).ready(appendChildToNav);
 function appendChildToNav() {
     const appendChildClass = 'cmp-navigation__item' + ' ' + 'cmp-navigation__item--level-1';
     if ($(window).width() <= 1023) {
-        const parentObject = document.getElementsByClassName('cmp-navigation__item--level-0');
+    const parentObject = document.getElementsByClassName('cmp-navigation__item--level-0');
         [...parentObject].forEach((parent, i) => {
             if (parent.lastElementChild.classList.contains('cmp-navigation__group')) {
-                console.log(parent.lastElementChild)
+    console.log(parent.lastElementChild)
                 const copyParent = parent.children[1].cloneNode(true);
                 copyParent.removeAttribute('style');
                 let parentLi = document.createElement("li");
-                if(!parent.lastElementChild.firstElementChild.classList.contains('hide--desktop')){
+    if(!parent.lastElementChild.firstElementChild.classList.contains('hide--desktop')){
                 parentLi.className = 'cmp-navigation__item cmp-navigation__item--level-1 hide--desktop';
                 parentLi.append(copyParent)
                 parent.lastElementChild.prepend(parentLi);
-                appendChildOnce = true;
+    appendChildOnce = true;
             }
-            }
+}
         });
 
     }
