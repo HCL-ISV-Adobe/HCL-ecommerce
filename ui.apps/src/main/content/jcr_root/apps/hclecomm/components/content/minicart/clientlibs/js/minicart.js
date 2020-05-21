62b92ff4cc21f21c0c1a0f95ad720018c3a5d1a8
AyushHCL <ayushmishraims@gmail.com>
2020-05-21 13:07:05 +0530

    making feedback changes

--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/minicart/clientlibs/js/minicart.js
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/minicart/clientlibs/js/minicart.js
@@ -1,33 +1,49 @@
-
-
-$(document).ready(function(){
+$(document).ready(function() {
     let cartId = '';
+    const getCountEle = document.getElementById("total-item-count");
+    getCountEle.style.display = 'none';
     document.cookie = "cartId = RntVBlSqoTTYDj3BepTMQybULNvY6Wbl";
-   // const cookiesCartID = document.cookie.split(';');
-   // console.log(parts);
     const cookiesCartID = ['cartId=RntVBlSqoTTYDj3BepTMQybULNvY6Wbl']
-    if(cookiesCartID && cookiesCartID.length > 0){
-   
-    cookiesCartID.forEach(function(cookiesCartIDItem){
-    if((cookiesCartIDItem).indexOf(cartId) > -1){
-    const cartIdArr = cookiesCartIDItem.split('=')
-    cartId  = cartIdArr[1];
-    }
-        console.log(cartId);
-    })
+    if (cookiesCartID && cookiesCartID.length > 0) {
 
-    if(cartId)
-    {
-        const xhttp = new XMLHttpRequest();
-  		xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-              document.getElementById("total-item-count").innerHTML =
-              this.responseText;
+        cookiesCartID.forEach(function(cookiesCartIDItem) {
+            if ((cookiesCartIDItem).indexOf(cartId) > -1) {
+                const cartIdArr = cookiesCartIDItem.split('=')
+                cartId = cartIdArr[1];
             }
-  		};
-           xhttp.open("GET", "/bin/hclecomm/cartsItems?cartId="+cartId, true);
-          xhttp.send();
-    }
+
+        })
+
+        if (cartId) {
+            const xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    if (this.responseText > 0) {
+                        getCountEle.style.display = 'inline-block';
+                        getCountEle.innerHTML =
+                            this.responseText;
+                    }
+                    else {
+            $("#cart-item-count").removeAttr("href");;
+                    }
+                }
+            };
+            xhttp.open("GET", "/bin/hclecomm/cartsItems?cartId=" + cartId, true);
+            xhttp.send();
+        }
 
     }
 });
+
+
+// method to show notification on empty cart 
+
+const redirectToCart = () => {
+    const getItemCount = $('.min-cart-text-count').text();
+    if (getItemCount === '0') {
+    $("#notification").empty();
+        $("#notification").css({display: 'inline-block'}).append('Cart is empty');
+        $("#notification").delay(5000).fadeOut("slow");
+    } 
+
+}
