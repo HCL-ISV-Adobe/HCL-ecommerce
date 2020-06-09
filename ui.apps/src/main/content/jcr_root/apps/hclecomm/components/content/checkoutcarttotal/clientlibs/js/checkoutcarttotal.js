f8c2bbdb931961d5121152b3ab6bee517cc575ab
AyushHCL <ayushmishraims@gmail.com>
2020-06-09 19:44:16 +0530

    working on checkou-total

--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/checkoutcarttotal/clientlibs/js/checkoutcarttotal.js
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/checkoutcarttotal/clientlibs/js/checkoutcarttotal.js
@@ -2,13 +2,21 @@ $(document).ready(function () {
 
     const getPriceLabel = $('.product-price').text();
 let target = document.querySelector(".product-detail");
-const getPorductDetails = JSON.parse(localStorage.getItem('productDescription'));
 
-let prodcutDeatilsEle = getPorductDetails['cartdetails'].map((item) => {
+    /*const getPorductDetails = {cartdetails : [{cartItem:{image: "https://www.hcltech.com/sites/default/files/styles/large/public/images/guideline_based1.png",
+														price: "54", qty: "3",title: "Expedition Tech Long-Sleeved Shirt"
+                                                        }}, {cartItem:{image: "https://www.hcltech.com/sites/default/files/styles/large/public/images/guideline_based1.png",
+														price: "54", qty: "3",title: "Expedition Tech Long-Sleeved Shirt"
+                                                        }}], coupondiscount: "$0", delivercharges: "16",fprice: "192.00"}*/
+    if(getPorductDetails){
+   		$('.total-price').text(getPorductDetails['fprice']);
+        $('.delivery-charges').text(getPorductDetails['delivercharges']) 
+		 $('.discount-price').text(getPorductDetails['coupondiscount'])
+		let prodcutDeatilsEle = getPorductDetails['cartdetails'].map((item) => {
             return (
                 `<div class="product-item-container">
                                                               			<div>
-                                                             			<img src= ${item['cartItem'].image}/>
+                                                             			<img class ='check-out-total-product-img' src= ${item['cartItem'].image}/>
                                                                          </div>
                                                              <div>
                                                                          <div>
@@ -16,13 +24,13 @@ let prodcutDeatilsEle = getPorductDetails['cartdetails'].map((item) => {
                                                                              <span>${item['cartItem'].title}</span>
                                                                          </div>
                                                                           <div>
-                                                                             <span>${item['cartItem'].qty}</span>
-                                                                         </div>
-                                                                        <div>
-                                                                             <span>${item['cartItem'].price}</span>
+                                                                             <span>${item['cartItem'].qty} * <span>$ ${item['cartItem'].price}</span></span>
                                                                          </div>
 
- 
+                                                             			<div><span>Price</span>
+                                                                             <span>${item['cartItem'].qty *  item['cartItem'].price}</span>
+                                                                         </div>
+																		<hr>
 
                                                                 </div>
                                                              </div>`
@@ -37,6 +45,6 @@ let prodcutDeatilsEle = getPorductDetails['cartdetails'].map((item) => {
             target.innerHTML = prodcutDeatilsEle;
 
         }
-
+}
 
 });
