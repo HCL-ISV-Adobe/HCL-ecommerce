f23695e8fd6970efb399e2c67615625ed19c4366
DeepaliAgrwl <deepalia@hcl.com>
2020-06-10 13:04:25 +0530

    confirmation component changes

--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/confirmation/clientlibs/js/confirmation.js
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/confirmation/clientlibs/js/confirmation.js
@@ -1,89 +1,87 @@
-let totalBagPrice = Number($('.total-bag-count').text());
-let bagDiscount = Number($('.bag-discount-amount').text());;
-let deliveryCharges = Number($('.delivery-charges').text());
-let placeOrderRedirection = null;
-let discount = 0;
-let discountedPercentage = discount/100;
 
-let allowCouponOnce = true;
+$(document).ready(function () {
 
-function onApplyCoupon(){
+    const getPorductDetails = JSON.parse(localStorage.getItem('productDescription'));
+	const checkOutDeatils = JSON.parse(localStorage.getItem('checkOutDetails'));
 
-    const getApppliedCoupon = $('.cart-detail-container__apply-coupon').val();
-	if(getApppliedCoupon === ''){
-        $('.apply-coupon-validation').text('Please enter a coupon').css("color", "red");
-        if(!allowCouponOnce)
-       		removeCoupon();
-			return;
+    if(checkOutDeatils && checkOutDeatils['orderId'])
+    {
+		$(".confirmation-failure").addClass("toggle-order");
+
+    let target = document.querySelector(".cmp-confirmation-productInfo");
+    let subTotal = document.querySelector(".cmp-confirmation-total");
+    let orderNumber = document.querySelector(".cmp-confirmation-orderNo");
+    let customerName = document.querySelector(".cmp-confirmation-section-customer-name");
+    let customerADD = document.querySelector(".cmp-confirmation-section-customer-address");
+    let customerCradDeatils = document.querySelector(".cmp-confirmation-subhead-card-details");
+    if (getPorductDetails && checkOutDeatils) {
+        let prodcutDeatilsEle = getPorductDetails['cartdetails'].map((item) => {
+            return (
+                `<div class="product-item-container">
+                                                              			<div>
+                                                             			<img class ='check-out-total-product-img' src= ${item['cartItem'].image}/>
+                                                                         </div>
+                                                             <div>
+                                                                         <div>
+                                                          					<span>Product:}</span>
+                                                                             <span>${item['cartItem'].title}</span>
+                                                                         </div>
+                                                                          <div>
+                                                                             <span>${item['cartItem'].qty} * <span>$ ${item['cartItem'].price}</span></span>
+                                                                         </div>
+                                                             			<div><span>Price</span>
+                                                                             <span>${item['cartItem'].qty *  item['cartItem'].price}</span>
+                                                                         </div>
+																		<hr>
+                                                                </div>
+                   </div>`
+            );
+
+ 
+
+        }).join('');
+
+ 
+
+        const fiNalAmount = `<div class="product-item-container">
+                                                                         <div>
+                                                                           Coupon Discount
+                                                                         </div>
+                                                                          <div>
+                                                                             <span>${getPorductDetails['coupondiscount']}</span>
+                                                                         </div>
+
+ 
+
+
+                                                                </div>`
+
+
+
+        const getOrderId = `<span>We have received your order</span> <span>Order No : ${checkOutDeatils['orderId']}</span>`
+        const getCustomerName = `<div class ='cmp-confirmation-subHead'>Delivery For </div> <div class ='cmp-confirmation-text'> ${checkOutDeatils['firstname']}  ${checkOutDeatils['lastname']}</div>`
+        const getCustomerAdd = `<div class ='cmp-confirmation-subHead'>Address </div> <div class ='cmp-confirmation-text'> ${checkOutDeatils['street']}  ${checkOutDeatils['city']} ${checkOutDeatils['postcode']}</div>`
+        const getCradDeatils = `
+                        <div>Card Number: ${checkOutDeatils['cardNumber']}</div>
+                            <div>Expiry Date: ${checkOutDeatils['cardExpDate']}</div>
+                            <div>Amount: ${getPorductDetails['fprice']}</div>`
+        if (prodcutDeatilsEle && fiNalAmount && getOrderId && target) {
+            target.innerHTML = prodcutDeatilsEle;
+            subTotal.innerHTML = fiNalAmount;
+            orderNumber.innerHTML = getOrderId;
+            customerName.innerHTML = getCustomerName;
+            customerADD.innerHTML = getCustomerAdd;
+            customerCradDeatils.innerHTML = getCradDeatils;
+        }
+
+ 
+
     }
-            const xhttp = new XMLHttpRequest();
-    		discount = "";
-            xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                if(this.responseText)
-                     discount = this.responseText;
-                	 removeCoupon();
-                     if(discount)
-                    {
-                            discountedPercentage = discount/100;
-                            const calculatedPrice = (totalBagPrice * (1-discountedPercentage)) - bagDiscount;
-                            const discountedPrice = (totalBagPrice * discountedPercentage );
-                            $('.coupon-discount').css("display","flex");
-                            $('.coupon-discount-amount').text(discountedPrice.toFixed(2));
-                            $('.order-price').text(calculatedPrice.toFixed(2));
-                            $('.total-price').text((calculatedPrice + deliveryCharges).toFixed(2));
-                            $('.apply-coupon-validation').text('Coupon Applied Sucessfully').css("color", "green");
-                            allowCouponOnce = false;
-                    }
-                    else{
-                        $('.apply-coupon-validation').text('Your Coupon is not valid').css("color", "red");
-                         if(!allowCouponOnce)
-                            removeCoupon();
-                    }
-    }
-  };
- 	xhttp.open("GET", "/bin/hclecomm/applyCoupon?coupon=" +  getApppliedCoupon, true);
-    xhttp.send();
 }
+ else
+ {
+	$(".cmp-confirmation").addClass("toggle-order");
+	$(".confirmation-failure").removeClass("toggle-order");
 
-
-$(document).ready(function (){
-   	 $('.order-price').text(totalBagPrice - bagDiscount);
-     $('.total-price').text(totalBagPrice - bagDiscount + deliveryCharges);
-	 placeOrderRedirection = $('.place-order-button').children().children().attr('href');
-	 $('.place-order-button').children().children().removeAttr("href");
-
-    if(Number($('.bag-discount-amount').text())>0)
-		$('.bag-discount').css("display","flex");
-    else
-		$('.bag-discount').css("display","none");
-
-    if(Number($('.coupon-discount-amount').text())>0)
-		$('.coupon-discount').css("display","flex");
-    else
-		$('.coupon-discount').css("display","none");
+ }
 });
-
-function onClickUpdateItem() {
-
-	const object = {"cartItem":{"quote_id": "AR93aupnz6KYQL786ZEdOAtEtL73lYQq","item_id": 5, "sku": "24-MB01", "qty": 20}}
-	const xhttp = new XMLHttpRequest();
-  	xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        placeOrderRedirection ? window.location.href = placeOrderRedirection : null;
-    }
-  };
- 	xhttp.open("GET", "/bin/hclecomm/updateCartItems?payload=" +  JSON.stringify(object) , true);
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.send();
-
-}
-
-function removeCoupon()
-{
-			const calculatedPrice = totalBagPrice - bagDiscount;
-            $('.coupon-discount').css("display","none");
-            $('.order-price').text(calculatedPrice);
-            $('.total-price').text(calculatedPrice + deliveryCharges);
-			allowCouponOnce = true;
-}
