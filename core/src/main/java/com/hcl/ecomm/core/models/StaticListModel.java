8afe3d37ed793bb37437ee7562bf90ea136122ee
rajamouli <rajamoulidudekula@hcl.com>
2020-06-23 17:48:18 +0530

    updated list slingmodel

--- core/src/main/java/com/hcl/ecomm/core/models/StaticListModel.java
+++ core/src/main/java/com/hcl/ecomm/core/models/StaticListModel.java
@@ -1,6 +1,8 @@
 package com.hcl.ecomm.core.models;
 
+import com.hcl.ecomm.core.pojo.ListPojo;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Optional;
 import org.slf4j.Logger;
@@ -9,38 +11,46 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 @Model(adaptables = Resource.class)
 public class StaticListModel {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Inject @Optional
-    private List<Resource> staticList;
-    private List<Listmodel> listItems = new ArrayList<>();
+    private  Resource staticList;
 
-    public List<Listmodel> getListItems() {
-        return listItems;
-    }
+    private List<ListPojo> listItems;
 
-    public void setListItems(List<Listmodel> listItems) {
-        this.listItems = listItems;
-    }
+
 
     @PostConstruct
     protected void init() {
-        logger.debug("In init of StaticListModel");
+        logger.debug("In init of StaticListFilterModel");
         try {
-            if (!staticList.isEmpty()) {
-                for (Resource resource : staticList) {
-                    Listmodel staticlist = resource.adaptTo(Listmodel.class);
-                    listItems.add(staticlist);
+            if(null!= staticList && staticList.hasChildren() )
+            {
+                listItems = new ArrayList<>();
+                Iterator<Resource> listChildren= staticList.listChildren();
+                while(listChildren.hasNext())
+                {
+                    ListPojo bean=new ListPojo();
+                    Resource resource=listChildren.next();
+                    ValueMap valueMap= resource.getValueMap();
+                    bean.setListitem(valueMap.get("itemList", String.class));
+                    listItems.add(bean);
                 }
             }
+
         }catch (Exception e){
             logger.error("Exception encountered",e.getMessage());
         }
 
-
     }
+
+    public List<ListPojo> getListItems() {
+        return listItems;
+    }
+
 }
 
