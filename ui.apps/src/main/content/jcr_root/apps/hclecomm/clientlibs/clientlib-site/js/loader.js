f8a0a036082b0e68982d44ded5b98ce2d0f3636e
DeepaliAgrwl <deepalia@hcl.com>
2020-06-23 17:42:26 +0530

    Loader Functionality committed

---
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/clientlibs/clientlib-site/js/loader.js
@@ -0,0 +1,14 @@
+
+function loader(flag){
+    let body=document.getElementsByTagName("body")[0];
+    
+    if(flag===true){
+        body.style.overflow="hidden";
+        console.log(" top",  window.scrollY);
+        body.insertAdjacentHTML('beforeend', `<div class="overlay" style="height:${window.innerHeight}px; top:${window.scrollY}px"><div class="loader"></div></div>`);
+    }else{
+        body.style.removeProperty("overflow");
+        let overlay= document.querySelector(".overlay");
+        body.removeChild(overlay);
+    }
+}
