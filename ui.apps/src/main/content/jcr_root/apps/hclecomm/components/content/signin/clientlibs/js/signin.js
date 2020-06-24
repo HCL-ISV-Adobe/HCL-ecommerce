d92c17dee0a81fff603fcd18e45225b6230bed6a
DeepaliAgrwl <deepalia@hcl.com>
2020-06-24 14:18:18 +0530

    Console Issue Fix

--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/signin/clientlibs/js/signin.js
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/signin/clientlibs/js/signin.js
@@ -12,34 +12,6 @@
 		return redirectURL;
 	}
 
-  const setUserCookie = function(cname,cvalue,exdays) {
-	let d = new Date();
-	d.setTime(d.getTime() + (exdays*24*60*60*1000));
-	const expires = "expires=" + d.toGMTString();
-	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-
-  const getUserCookie = function(cname) {
-	let name = cname + "=";
-	let decodedCookie  = decodeURIComponent(document.cookie);
-	let ca = decodedCookie.split(';');
-	for(let i = 0; i < ca.length; i++) {
-	  let c = ca[i].trim();
-	  if(c.indexOf(name) === 0){
-		return c.substring(name.length, c.length);
-	  }
-	}
-	return "";
-  }
-
-  const checkUserCookie = function(cname) {
-	if(getUserCookie("hcluser") != "") {
-	  return true;
-	} else {
-	  return false;
-	}
-  }
-
   let userData = getUserCookie("hcluser");
     if(userData != "") {
          custToken = JSON.parse(userData).customerToken;
