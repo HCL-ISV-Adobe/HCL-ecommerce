4fb491e114fc88b417192e8233383f45a20e3c2a
rakeshkumar-verma <rakeshkumar.verma@hcl.com>
2020-06-25 16:08:12 +0530

    mobile view issue for language nav fixed

--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/languagenavigation/clientlibs/js/languagenavigation.js
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/languagenavigation/clientlibs/js/languagenavigation.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
     const selectedLanguage = $(".cmp-languagenavigation__item--active > a").text();
     $(".language-navigation-btn span").text(selectedLanguage);
     $(".language-navigation-btn").click(function () {
-        $(".language-navigation-btn .fa").hasClass("fa-angle-down") ?
-            ($(".language-navigation-btn .fa").addClass("fa-angle-up").removeClass("fa-angle-down")) :
-            ($(".language-navigation-btn .fa").addClass("fa-angle-down").removeClass("fa-angle-up"));
+        $(".language-navigation-btn .fa-drpdwn").hasClass("fa-angle-down") ?
+            ($(".language-navigation-btn .fa-drpdwn").addClass("fa-angle-up").removeClass("fa-angle-down")) :
+            ($(".language-navigation-btn .fa-drpdwn").addClass("fa-angle-down").removeClass("fa-angle-up"));
         $(".cmp-languagenavigation").toggleClass("show");
     });
 });
