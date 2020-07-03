43cc881ea2cfe884030c2158ad51142654f6bef3
ReenaOmana <65391905+ReenaOmana@users.noreply.github.com>
2020-07-03 17:58:16 +0530

    Static List test

--- core/src/test/java/com/hcl/ecomm/core/models/StaticListModelTest.java
+++ core/src/test/java/com/hcl/ecomm/core/models/StaticListModelTest.java
@@ -1,5 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.hcl.ecomm.core.models;
+import com.hcl.ecomm.core.pojo.ListPojo;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
 
 class StaticListModelTest {
 
+ ListPojo lp=new ListPojo();
+ List<ListPojo>listPojos=new ArrayList<>();
+ StaticListModel sm=new StaticListModel();
+
+
+    @Test
+    public void getListItems(){
+        lp.setListitem("page1");
+        listPojos.add(lp);
+        sm.getListItems();
+    }
 }
