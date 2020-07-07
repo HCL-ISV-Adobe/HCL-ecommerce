469a2835f6a65f49c4c7c2c664beb1253026a09f
DeepaliAgrwl <deepalia@hcl.com>
2020-07-07 12:32:06 +0530

    form button and text componext proxy structure

---
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/form/button/clientlibs/js/button.js
@@ -0,0 +1,21 @@
+
+(function($) {
+    "use strict";
+
+    var BUTTON_NAME = ".cmp-form-button__editor-name";
+    var BUTTON_VALUE = ".cmp-form-button__editor-value";
+    var PROP_ERROR_MESSAGE = "error-message";
+
+    $.validator.register({
+        selector: BUTTON_NAME,
+        validate: function(el) {
+            var valueInput = el.closest("form").find(BUTTON_VALUE);
+            if (valueInput.val() !== "") {
+                if (el.val() === "") {
+                    return el.data(PROP_ERROR_MESSAGE);
+                }
+            }
+        }
+    });
+
+})(jQuery);
