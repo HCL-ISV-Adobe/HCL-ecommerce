e99cd27f313f2b9d02052482bcbe8c82d33e4456
ReenaOmana <65391905+ReenaOmana@users.noreply.github.com>
2020-07-08 12:07:14 +0530

    Updated Test case for StaticListModelTest.java

--- core/src/test/java/com/hcl/ecomm/core/models/StaticListModelTest.java
+++ core/src/test/java/com/hcl/ecomm/core/models/StaticListModelTest.java
@@ -1,21 +1,33 @@
 package com.hcl.ecomm.core.models;
+
 import com.hcl.ecomm.core.pojo.ListPojo;
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.apache.sling.api.resource.Resource;
 import org.junit.jupiter.api.Test;
-import java.util.ArrayList;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(AemContextExtension.class)
 class StaticListModelTest {
 
- ListPojo lp=new ListPojo();
- List<ListPojo>listPojos=new ArrayList<>();
- StaticListModel sm=new StaticListModel();
-
+    private Resource resource;
+    private StaticListModel staticListModel;
+    private final AemContext context = new AemContext();
 
     @Test
-    public void getListItems(){
-        lp.setListitem("page1");
-        listPojos.add(lp);
-        sm.getListItems();
+    void getListItems() {
+        context.addModelsForClasses(StaticListModel.class);
+        context.load().json("/StaticListModel.json", "/content");
+        context.currentPage("/content/ListPage");
+        resource = context.currentResource("/content/ListPage/jcr:content/root/responsivegrid/list");
+        if (resource != null)
+            staticListModel = resource.adaptTo(StaticListModel.class);
+        assert staticListModel != null;
+        List<ListPojo> actual = staticListModel.getListItems();
+        assertFalse(actual.isEmpty());
     }
 }
