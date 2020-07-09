6694b19bb4c4462193fe689ff86d9d7a81ff59bb
velhcl <67053984+velhcl@users.noreply.github.com>
2020-07-09 23:07:19 +0530

    style changes

--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/contactus/clientlibs/js/contactus.js
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/contactus/clientlibs/js/contactus.js
@@ -28,7 +28,6 @@ function validateContactFrom(e) {
     ErrorMsgElm.innerHTML = "";
     ErrorMsgElm.style.visibility = "hidden";
     let errorhtml = "";
-    //Cannot read property 'forEach' of undefined
     for(let i=0; i<document.contactus_form.elements.length;i++){
         document.contactus_form.elements[i].style.borderColor = "#d1d1d1";
         if (document.contactus_form.elements[i].required && document.contactus_form.elements[i].value === '') {
@@ -44,90 +43,10 @@ function validateContactFrom(e) {
             return false;
         }
     }
-    // document.contactus_form.elements.forEach(element => {
-    //     if (element.required) {
-    //         errorhtml = '<span> Please enter '+element.name+'<span>';
-    //         displayError(errorhtml, element.name);
-    //         isValid = false;
-    //         return false;
-    //     }       
-
-    // });
   
     if(isValid){
         debugger;
         return true;
     }
-    // var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
-    // var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    // var phoneno = /^\d{10}$/;
-    // debugger;
-    // if (document.signup_form.firstname.value == "") {
-    //     errorhtml = '<span>Please enter First Name<span>';
-    //     displayError(errorhtml, 'firstname');
-    //     return false;
-    // }
-    // if (document.signup_form.lastname.value == "") {
-    //     errorhtml = '<span>Please enter Last Name<span>';
-    //     displayError(errorhtml, 'lastname');
-    //     return false;
-    // }
-    // if (document.signup_form.email.value == "") {
-    //     errorhtml = '<span>Please enter Email<span>';
-    //     displayError(errorhtml, 'email');
-    //     return false;
-    // } else if (!document.signup_form.email.value.match(mailformat)) {
-    //     errorhtml = '<span>Please enter Email in proper format<span>';
-    //     displayError(errorhtml, 'email');
-    //     return false;
-    // }
-    // if (document.signup_form.phone.value == "") {
-    //     errorhtml = '<span>Please enter Phone Number<span>';
-    //     displayError(errorhtml, 'phone');
-    //     return false;
-    // } else if (!document.signup_form.phone.value.match(phoneno)) {
-    //     errorhtml = '<span>Phone number must be 10 digits<span>';
-    //     displayError(errorhtml, 'phone');
-    //     return false;
-    // }
-    // if (document.signup_form.password.value == "") {
-    //     errorhtml = '<span>Please enter Password<span>';
-    //     displayError(errorhtml, 'password');
-    //     return false;
-    // } else if (!document.signup_form.password.value.match(passw)) {
-    //     errorhtml = '<span>Password must contain at least one numeric digit, one uppercase and one lowercase letter and lenght 8 to 10<span>';
-    //     displayError(errorhtml, 'password');
-    //     return false;
-    // }
-    // if (document.signup_form.cfpassword.value == "") {
-    //     errorhtml = '<span>Please enter Confirm password <span>';
-    //     displayError(errorhtml, 'cfpassword');
-    //     return false;
-    // }
-    // if (document.signup_form.password.value !== document.signup_form.cfpassword.value) {
-    //     errorhtml = '<span>Confirm password should be same as password<span>';
-    //     displayError(errorhtml, 'cfpassword');
-    //     return false;
-    // }
-    // if (document.signup_form.terms.checked === false) {
-    //     errorhtml = '<span>Please accept terms and conditions<span>';
-    //     displayError(errorhtml, 'terms');
-    //     return false;
-    // }
-
-
-    // const formData = {
-    //     firstname: document.signup_form.firstname.value,
-    //     email: document.signup_form.email.value,
-    //     phone: document.signup_form.phone.value,
-    //     password: document.signup_form.password.value
-    // }
-    // formData.lastname = (document.signup_form.lastname.value) ? document.signup_form.lastname.value : "";
-
-
-    // let url = '/bin/hclecomm/customerSignup';
-    // await handleHttpServerRequestJson2(url,formData);
-
-
     
 }
