0a209619cd58401bf379d4502837eb1604956a67
velhcl <67053984+velhcl@users.noreply.github.com>
2020-07-10 16:57:44 +0530

    validation added

--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/contactus/clientlibs/js/contactus.js
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/contactus/clientlibs/js/contactus.js
@@ -1,21 +1,21 @@
 
-if(document.contactus_form){
+if (document.contactus_form) {
     let formLabels = document.contactus_form.querySelectorAll('label');
-    for(let i=0; i<formLabels.length;i++){
+    for (let i = 0; i < formLabels.length; i++) {
         if (document.contactus_form.querySelectorAll('label')[i].nextElementSibling.required) {
-            let startHtml = document.createElement('span'); 
+            let startHtml = document.createElement('span');
             startHtml.classList.add('star');
             startHtml.innerHTML = '*';
-          document.contactus_form.querySelectorAll('label')[i].appendChild(startHtml);
+            document.contactus_form.querySelectorAll('label')[i].appendChild(startHtml);
         }
     }
 }
 function displayFormError(errorhtml, formElmName) {
-    const ErrorMsgElm = document.getElementById('cmp-contactus-errormsg');
     document.contactus_form[formElmName].focus();
-    ErrorMsgElm.style.visibility = "visible";
     document.contactus_form[formElmName].style.borderColor = "red";
-    ErrorMsgElm.innerHTML = errorhtml;
+    const errorElm = document.contactus_form[formElmName].parentElement.nextElementSibling;
+    errorElm.style.visibility = "visible";
+    errorElm.innerHTML = errorhtml;
     return;
 }
 
@@ -23,35 +23,38 @@ function validateContactFrom(e) {
     const passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
     const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     const phoneno = /^\d{10}$/;
-    const ErrorMsgElm = document.getElementById('cmp-contactus-errormsg');
-    let isValid = true;     
-    ErrorMsgElm.innerHTML = "";
-    ErrorMsgElm.style.visibility = "hidden";
+    debugger;
+    const errorMsgElm = document.getElementsByClassName("cmp-contactus-errormsg");
+    for (let i = 0; i < errorMsgElm.length; i++) {
+        errorMsgElm[i].innerHTML = "";
+        errorMsgElm[i].style.visibility = "hidden";
+    }
+    let isValid = true;
     let errorhtml = "";
-    for(let i=0; i<document.contactus_form.elements.length;i++){
+    for (let i = 0; i < document.contactus_form.elements.length; i++) {
         document.contactus_form.elements[i].style.borderColor = "#d1d1d1";
         if (document.contactus_form.elements[i].required && document.contactus_form.elements[i].value === '') {
-            errorhtml = '<span> Please enter '+document.contactus_form.elements[i].name+'<span>';
+            errorhtml = '<span> Please enter ' + document.contactus_form.elements[i].name + '<span>';
             displayFormError(errorhtml, document.contactus_form.elements[i].name);
             isValid = false;
             return false;
         }
-        if (document.contactus_form.elements[i].type =='email' && !document.contactus_form.elements[i].value.match(mailformat)){
+        if (document.contactus_form.elements[i].type == 'email' && !document.contactus_form.elements[i].value.match(mailformat)) {
             errorhtml = '<span> Please enter valid mail format <span>';
             displayFormError(errorhtml, document.contactus_form.elements[i].name);
             isValid = false;
             return false;
         }
-        if (document.contactus_form.elements[i].type =='tel' && !document.contactus_form.elements[i].value.match(phoneno)){
-		  errorhtml = '<span>Please enter a valid phone number<span>';
-		  displayFormError(errorhtml, document.contactus_form.elements[i].name);
+        if (document.contactus_form.elements[i].type == 'tel' && !document.contactus_form.elements[i].value.match(phoneno)) {
+            errorhtml = '<span>Please enter a valid phone number<span>';
+            displayFormError(errorhtml, document.contactus_form.elements[i].name);
             isValid = false;
             return false;
-	  }
+        }
     }
-  
-    if(isValid){
+
+    if (isValid) {
         return true;
     }
-    
+
 }
