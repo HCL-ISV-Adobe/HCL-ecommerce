4e98fb8c5d3986eaf6fa245155717465e68d3318
DeepaliAgrwl <deepalia@hcl.com>
2020-07-10 20:22:13 +0530

    prefilled email Id resolution

---
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/newsletter/clientlibs/js/newsletter.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    let userData = getUserCookie("hcluser");
+    let custEmail;
+    if(userData != "") {
+         custEmail = JSON.parse(userData).email;
+        document.querySelector(".cmp-newsletter-form .text  input").value = custEmail;
+    }
+ )};
