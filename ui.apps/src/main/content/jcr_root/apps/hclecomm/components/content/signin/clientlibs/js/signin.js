8e24a5503936e511321393d21bbe5973774e9779
Nithesh kumar <nitheshkumar.reddy@hcl.com>
2020-07-20 16:40:28 +0530

    Signin Changes

--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/signin/clientlibs/js/signin.js
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/signin/clientlibs/js/signin.js
@@ -7,10 +7,10 @@ const findRedriectUrl = function(domId) {
 		if(params.has('referer')) {
 			if(!keys.some(k => params.get('referer').includes(k) )) {
 				redirectURL = params.get('referer');
-                if(!hasCart && redirectURL.includes('checkout.html'))
-                {
+				if(!hasCart && redirectURL.includes('checkout.html'))
+				{
 					redirectURL = domId.getAttribute("data-default");
-                }
+				}
 			}
 		} else if(domId.getAttribute("data-default")) {
 			redirectURL = domId.getAttribute("data-default");
@@ -19,126 +19,126 @@ const findRedriectUrl = function(domId) {
 	}
 
   let userData = getUserCookie("hcluser");
-    if(userData != "") {
-         custToken = JSON.parse(userData).customerToken;
-    }
+	if(userData != "") {
+		 custToken = JSON.parse(userData).customerToken;
+	}
 
 const handleHttpServerRequestJson = function (url, formdata) {
 			loader(true);
-            var othrParm = {
-              headers: {"content-type":"application/json; charset=UTF-8", 'Accept': 'application/json'},
-              body: JSON.stringify(formdata),
-              method: "POST"
-            }
-            fetch('/libs/granite/csrf/token.json')
-            .then(
-                (response) => {return response.json();},
-                (rejected) => {console.log(rejected);
-            })
-            .then( msg => {
-                    othrParm.headers['CSRF-Token'] = msg.token;
-                        return fetch(url, othrParm);
-            })
-            .then(
-                (response) => {return response.json();},
-                (rejected) => {console.log(rejected);
-                 failedLoginCounter++;
-                               if(failedLoginCounter > 1) {
-                                   document.getElementById('recaptcha').style.visibility = "visible";
-                               }
-            })
-            .then(data => {
+			var othrParm = {
+			  headers: {"content-type":"application/json; charset=UTF-8", 'Accept': 'application/json'},
+			  body: JSON.stringify(formdata),
+			  method: "POST"
+			}
+			fetch('/libs/granite/csrf/token.json')
+			.then(
+				(response) => {return response.json();},
+				(rejected) => {console.log(rejected);
+			})
+			.then( msg => {
+					othrParm.headers['CSRF-Token'] = msg.token;
+						return fetch(url, othrParm);
+			})
+			.then(
+				(response) => {return response.json();},
+				(rejected) => {console.log(rejected);
+				 failedLoginCounter++;
+							   if(failedLoginCounter > 1) {
+								   document.getElementById('recaptcha').style.display = "block";
+							   }
+			})
+			.then(data => {
 				loader(false);
-                const status = (data.status)?JSON.parse(data.status): false;
-                if(status === true) {
-                  failedLoginCounter = 0;
-                  document.getElementById('recaptcha').style.visibility = "hidden";
-                  if(checkUserCookie("hcluser") === false){
-                    const exdays = (formdata.rememberme)?5:1;
-                    setUserCookie("hcluser",JSON.stringify(data.message),exdays);
-                               setUserCookie("cartId","",-1);
-                        custToken = data.message.customerToken;
-                        const xhttp = new XMLHttpRequest();
-                   		 xhttp.onreadystatechange = function () {
-                        if (this.readyState == 4 && this.status == 200) {
-                            if(this.responseText)
-                            {
-			       const data = JSON.parse(this.responseText)
-                               if(data.status == 200) {
-                                   const cartItem = [];
-                                   let cartId = "";
-                                   const cartdetails = [];
-                                   let totalPrice = 0;
-                               		const cartDetail = JSON.parse(data.message);
-                                   	if(cartDetail.length!=0){
-                               			hasCart = true;
-                                        cartDetail.forEach((item) =>{
-                                            console.log(item.name);
-                                            const itemObj = {
-                                                "image":"https://www.hcltech.com/sites/default/files/styles/large/public/images/guideline_based1.png",
-                                                "title":item.name,
-                                                "qty" : item.qty,
-                                                "price" :item.price
-                                            }
-                                            totalPrice = totalPrice + (item.qty * item.price);
-                                            cartId = item.quote_id;
-                                            cartdetails.push({cartItem : itemObj})
+				const status = (data.status)?JSON.parse(data.status): false;
+				if(status === true) {
+				  failedLoginCounter = 0;
+				  document.getElementById('recaptcha').style.display = "none";
+				  if(checkUserCookie("hcluser") === false){
+					const exdays = (formdata.rememberme)?5:1;
+					setUserCookie("hcluser",JSON.stringify(data.message),exdays);
+							   setUserCookie("cartId","",-1);
+						custToken = data.message.customerToken;
+						const xhttp = new XMLHttpRequest();
+						 xhttp.onreadystatechange = function () {
+						if (this.readyState == 4 && this.status == 200) {
+							if(this.responseText)
+							{
+				   const data = JSON.parse(this.responseText)
+							   if(data.status == 200) {
+								   const cartItem = [];
+								   let cartId = "";
+								   const cartdetails = [];
+								   let totalPrice = 0;
+									const cartDetail = JSON.parse(data.message);
+									if(cartDetail.length!=0){
+										hasCart = true;
+										cartDetail.forEach((item) =>{
+											console.log(item.name);
+											const itemObj = {
+												"image":"https://www.hcltech.com/sites/default/files/styles/large/public/images/guideline_based1.png",
+												"title":item.name,
+												"qty" : item.qty,
+												"price" :item.price
+											}
+											totalPrice = totalPrice + (item.qty * item.price);
+											cartId = item.quote_id;
+											cartdetails.push({cartItem : itemObj})
 
-                                        })
+										})
 
-                                        document.cookie = "cartId = " + cartId + "; path=/";
-                                        const fprice= totalPrice;
-                                        const coupondiscount="0";
-                                        const delivercharges="0";
+										document.cookie = "cartId = " + cartId + "; path=/";
+										const fprice= totalPrice;
+										const coupondiscount="0";
+										const delivercharges="0";
 
 
-                                        const productDescription = {
-                                            cartdetails,
-                                            fprice,
-                                            coupondiscount,
-                                            delivercharges
-                                        }
+										const productDescription = {
+											cartdetails,
+											fprice,
+											coupondiscount,
+											delivercharges
+										}
 
-                                        localStorage.setItem('productDescription', JSON.stringify(productDescription ))
-                                 }
+										localStorage.setItem('productDescription', JSON.stringify(productDescription ))
+								 }
 
-                              }
-                              else if(data.status == 404)
-                              {
+							  }
+							  else if(data.status == 404)
+							  {
 									console.log('No cart Id present');
-                                    localStorage.removeItem('productDescription');
-                                    localStorage.removeItem('checkOutDetails');
-                                    $('.checkouttotal-cmp').text("The Cart is empty");
-                              }
-                            }
+									localStorage.removeItem('productDescription');
+									localStorage.removeItem('checkOutDetails');
+									$('.checkouttotal-cmp').text("The Cart is empty");
+							  }
+							}
 							setTimeout(function(){window.location = findRedriectUrl(document.login_form);}, 1000);
-                        }
-                		};
-                        xhttp.open("GET", "/bin/hclecomm/getCustomerCart", true);
-                        xhttp.setRequestHeader("CustomerToken", custToken);
-                        xhttp.send();
-                  }
+						}
+						};
+						xhttp.open("GET", "/bin/hclecomm/getCustomerCart", true);
+						xhttp.setRequestHeader("CustomerToken", custToken);
+						xhttp.send();
+				  }
 		} else {
-                  let error = "Server status failed. ";
-                  failedLoginCounter++;
-                                            if(failedLoginCounter > 1) {
-                                   document.getElementById('recaptcha').style.visibility = "visible";
-                               }
-                  if(data.message.error) {
-                    console.log(data.message.error);
-                     error = data.message.error;
-                  }
-                  console.log(error);
-                  const errorElm = document.getElementById('cmp-login-errormsg');
-                  errorElm.style.visibility = "visible";
-                  errorElm.innerHTML  = '<span>'+ error+ '</span>';
-                }
-            })
-            .catch((error) => {
-              console.log('promise error',error);
-            });
+				  let error = "Server status failed. ";
+				  failedLoginCounter++;
+											if(failedLoginCounter > 1) {
+								   document.getElementById('recaptcha').style.display = "block";
+							   }
+				  if(data.message.error) {
+					console.log(data.message.error);
+					 error = data.message.error;
+				  }
+				  console.log(error);
+				  const errorElm = document.getElementById('cmp-login-errormsg');
+				  errorElm.style.visibility = "visible";
+				  errorElm.innerHTML  = '<span>'+ error+ '</span>';
+				}
+			})
+			.catch((error) => {
+			  console.log('promise error',error);
+			});
 
-        }
+		}
 
   async function validateLoginFrom(e) {
 	const ErrorMsgElm = document.getElementById('cmp-login-errormsg');
@@ -154,11 +154,11 @@ const handleHttpServerRequestJson = function (url, formdata) {
 	}
 	if(validateRecaptcha() == false && failedLoginCounter > 1) {
 		ErrorMsgElm.style.visibility = "visible";
-        ErrorMsgElm.innerHTML = "<span>please verify you are humann!<span>";
-          //errorhtml = '<span>please verify you are humann!<span>';
-          //displayError(errorhtml, 'terms');
-        return false;
-    }
+		ErrorMsgElm.innerHTML = "<span>please verify you are humann!<span>";
+		  //errorhtml = '<span>please verify you are humann!<span>';
+		  //displayError(errorhtml, 'terms');
+		return false;
+	}
 	const data = {
 		username: document.login_form.username.value,
 		password: document.login_form.password.value,
