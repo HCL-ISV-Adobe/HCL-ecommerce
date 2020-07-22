2c21d018230e0d225a0493534d1036cbf84809d1
ShubhamMittal <shubhamm@hcl.com>
2020-07-22 23:32:13 +0530

    Added Null checks

--- core/src/main/java/com/hcl/ecomm/core/servlets/ProductDetailsServlet.java
+++ core/src/main/java/com/hcl/ecomm/core/servlets/ProductDetailsServlet.java
@@ -57,8 +57,11 @@ public class ProductDetailsServlet extends SlingSafeMethodsServlet {
             if(stock_item.getAsJsonObject().get("is_in_stock") != null){
                 stock = stock_item.getAsJsonObject().get("is_in_stock").getAsString();
             }
-            if(stock_item.getAsJsonObject().get("qty") != null){
-                qty = stock_item.getAsJsonObject().get("qty").getAsString();
+            if(stock_item.getAsJsonObject().get("qty") != null ){
+                JsonElement quantity =  stock_item.getAsJsonObject().get("qty");
+               if(("null").equals(quantity)){
+                   qty = stock_item.getAsJsonObject().get("qty").getAsString();
+               }
             }
         }
         productMap.put("sku", skuId);
