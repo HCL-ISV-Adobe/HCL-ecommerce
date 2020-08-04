32848d75ac2505ce1a5974c83c72dcccf603f0c6
Nithesh kumar <nitheshkumar.reddy@hcl.com>
2020-08-04 16:27:56 +0530

    Committing the changes for rating story

---
+++ core/src/main/java/com/hcl/ecomm/core/servlets/RatingServlet.java
@@ -0,0 +1,4 @@
+package com.hcl.ecomm.core.servlets;
+
+public class RatingServlet {
+}
---
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/ratings/clientlibs/js/ratings.js
@@ -0,0 +1,7 @@
+let avgRating = 3.5;
+document.getElementById("rating" + avgRating * 2).checked = true;
+
+function submitRating(rating) {
+console.log(rating);
+}
+
