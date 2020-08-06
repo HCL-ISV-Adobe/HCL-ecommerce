cfb272a483e71131a50c82a5afae9edf3deab536
Nithesh kumar <nitheshkumar.reddy@hcl.com>
2020-08-06 12:41:38 +0530

    Backend Changes

--- core/src/main/java/com/hcl/ecomm/core/services/impl/AddToCartServiceImpl.java
+++ core/src/main/java/com/hcl/ecomm/core/services/impl/AddToCartServiceImpl.java
@@ -165,10 +165,4 @@ public class AddToCartServiceImpl implements AddToCartService{
 
 
 
-	
-
-
-
-
-
 }
--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/ratings/clientlibs/js/ratings.js
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/ratings/clientlibs/js/ratings.js
@@ -1,7 +1,36 @@
-let avgRating = 3.5;
-document.getElementById("rating" + avgRating * 2).checked = true;
+let avgRating = 0;
+
+
+const xhttpReq = new XMLHttpRequest();
+xhttpReq.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        let ratingResponse;
+        try {
+            ratingResponse = JSON.parse(this.responseText);
+
+            avgRating = Math.round(ratingResponse[0].rating * 2) / 2;
+            document.getElementById("rating" + avgRating * 2).checked = true;
+        } catch (e) {
+            console.log(e)
+        }
+    }
+};
+prodSku = $(".product-sku").text();
+xhttpReq.open("GET", "/bin/hclecomm/ratinglist?sku=" + prodSku, true);
+xhttpReq.send();
+
 
 function submitRating(rating) {
-console.log(rating);
+    let data = {
+        "sku": prodSku,
+        "name": prodName,
+        "rating": rating
+    }
+    //sending data to server
+    let xhttpPostReq = new XMLHttpRequest();
+    let url = "/bin/hclecomm/ratinglist";
+    xhttpPostReq.open("POST", url, true);
+    xhttpPostReq.setRequestHeader("Content-Type", "application/json");
+    xhttpPostReq.setRequestHeader("CustomerToken", custToken);
+    xhttpPostReq.send(JSON.stringify(data));
 }
-
