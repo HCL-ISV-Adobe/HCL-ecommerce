f355f79fd47e301fa94d5a63db5b63aaa9ee1313
Umesh Venugopal <umesh.venugopal@hcl.com>
2020-08-14 13:55:53 +0530

    Overflow content arrow button ui fix

--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/button/clientlibs/js/button.js
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/button/clientlibs/js/button.js
@@ -1,39 +1,13 @@
-$(function() {
-    /*let userData = getUserCookie("hcluser");
-    if(userData != "") {
-        let username = (JSON.parse(userData).firstname)?JSON.parse(userData).firstname:"User";
-        $('.signin').css('display','none');
-        $('.signup').css('display','none');
-        $('.logout').css('display','inline-block');
-        $('.myaccount').css('display','inline-block');
-        let html = '<span class="btn-cmp-logout-text">Hi, '+ username +'</span>';
-        const logoutIconElm = document.querySelector('.btn-cmp-logout-icon');
-        logoutIconElm.insertAdjacentHTML("beforebegin",html);
-        document.querySelector('.logout').addEventListener('click', function(){
-            setUserCookie("hcluser","",-1);
-            setUserCookie("cartId","",-1);
-            location.reload();
-        });
-    } else {
-         let currentWCMMode = getUserCookie("wcmmode");
-         if (currentWCMMode == "edit") {
-                    $('.logout').css('display', 'inline-block');
-                    $('.myaccount').css('display','inline-block');
-         }else{
-             $('.logout').css('display', 'none');
-             $('.myaccount').css('display','none');
-         }
-        $('.signin').css('display','inline-block');
-        $('.signup').css('display','inline-block');
-    }*/
-    
+$(function() {    
     const userLogout = function(){
         setUserCookie("hcluser","",-1);
         setUserCookie("cartId","",-1);
         location.reload();
     }
     
-    if(checkmode && checkmode === 'noEdit') {
+    //if(checkmode && checkmode == "noEdit") {
+    console.log('checkmode - buttonjs', checkmode)
+    if(checkmode !== "edit") {
         const userData = getUserCookie("hcluser");
         console.log(userData)
         if(userData) {
@@ -44,13 +18,15 @@ $(function() {
             $('.logout').css('display','inline-block');
             let html = '<span class="btn-cmp-logout-text">Hi, '+ customerName +'</span>';
             document.querySelector('.btn-cmp-logout-icon').insertAdjacentHTML("beforebegin",html)
+            $('.logout span.cmp-button__text').css('display','none');
             document.querySelector('.logout').addEventListener('click', userLogout);
-            
+
             $('.myaccount').css('display','inline-block');    
         } else {
             $('.signin').css('display','inline-block');
             $('.signup').css('display','inline-block');
             $('.logout').css('display', 'none');
+            //$('logout span.cmp-button__text').css('display','inline-block');
             $('.myaccount').css('display','none');
         }
     }
