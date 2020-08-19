e26dc36001a1e35e925e9abde52068ef9a350ab9
Umesh Venugopal <umesh.venugopal@hcl.com>
2020-08-19 08:11:34 +0530

    ui fix lang nav

--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/languagenavigation/clientlibs/js/languagenavigation.js
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/languagenavigation/clientlibs/js/languagenavigation.js
@@ -1,10 +1,25 @@
 $(document).ready(function () {
     const selectedLanguage = $(".cmp-languagenavigation__item--active > a").text();
     $(".language-navigation-btn span").text(selectedLanguage);
-    $(".language-navigation-btn").click(function () {
+
+    $(".language-navigation-btn").click( function (e) {
         $(".language-navigation-btn .fa-drpdwn").hasClass("fa-angle-down") ?
             ($(".language-navigation-btn .fa-drpdwn").addClass("fa-angle-up").removeClass("fa-angle-down")) :
             ($(".language-navigation-btn .fa-drpdwn").addClass("fa-angle-down").removeClass("fa-angle-up"));
+
         $(".cmp-languagenavigation").toggleClass("show");
+
+        e.stopPropagation();
     });
-});
+
+    //stop propagation for nav layer onclick event
+
+    window.onclick = function(e) {
+        if ( !e.target.matches('.language-navigation-btn') ) {
+            if($('.cmp-languagenavigation').hasClass('show')) {
+				$(".cmp-languagenavigation").removeClass("show");
+                $(".language-navigation-btn .fa-drpdwn").addClass("fa-angle-down").removeClass("fa-angle-up");
+            }
+        }
+    }
+});	
