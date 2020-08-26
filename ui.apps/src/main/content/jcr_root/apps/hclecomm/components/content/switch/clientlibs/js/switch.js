af672dede0879ae83a78a6ce14d4f737178205a7
Umesh Venugopal <umesh.venugopal@hcl.com>
2020-08-26 12:52:33 +0530

    adding switch component for toggle icon darktheme

---
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/switch/clientlibs/js/switch.js
@@ -0,0 +1,14 @@
+$(function() {
+var checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function() {
+    if(this.checked) {
+        //trans()
+        document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+        //trans()
+        document.documentElement.setAttribute('data-theme', 'light')
+    }
+});
+
+});
