f9590eb04d3a76da3b96f5c88102cd001e93b95f
Umesh Venugopal <umesh.venugopal@hcl.com>
2020-08-28 16:18:17 +0530

    darktheme mode stored in localstorage

--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/switch/clientlibs/js/switch.js
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/switch/clientlibs/js/switch.js
@@ -1,14 +1,32 @@
 $(function() {
-var checkbox = document.querySelector('input[name=theme]');
-
-checkbox.addEventListener('change', function() {
-    if(this.checked) {
-        //trans()
-        document.documentElement.setAttribute('data-theme', 'dark')
-    } else {
-        //trans()
-        document.documentElement.setAttribute('data-theme', 'light')
+    //var checkbox = document.querySelector('input[name=theme]');
+    var darkMode = localStorage.getItem('darkMode');
+    if(darkMode == 'enabled') {
+        document.documentElement.setAttribute('data-theme', 'dark');
     }
-});
+    //in principle will have only one component for one page.
+    var darkThemebtn = document.querySelectorAll('.dark-mode-toggle');
+    darkThemebtn.forEach(item => {
+        item.addEventListener('click', function(event) {
+            //console.log(event.target);
 
+            if(document.documentElement.getAttribute('data-theme') 
+                && document.documentElement.getAttribute('data-theme') == 'dark') {
+                document.documentElement.setAttribute('data-theme', 'light');
+                localStorage.setItem('darkMode', null);
+            } else {
+                document.documentElement.setAttribute('data-theme', 'dark');
+                localStorage.setItem('darkMode', 'enabled');
+            }
+
+            /*if(this.checked) {
+                //trans()
+                document.documentElement.setAttribute('data-theme', 'dark')
+            } else {
+                //trans()
+                document.documentElement.setAttribute('data-theme', 'light')
+            }*/
+
+        });
+    });
 });
