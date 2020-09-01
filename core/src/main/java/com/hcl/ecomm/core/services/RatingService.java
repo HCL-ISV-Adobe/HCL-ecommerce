f2a1035fd55acae80e7f0616011013634874f264
Nithesh kumar <nitheshkumar.reddy@hcl.com>
2020-09-01 15:13:25 +0530

    Rating service changes

--- core/src/main/java/com/hcl/ecomm/core/services/RatingService.java
+++ core/src/main/java/com/hcl/ecomm/core/services/RatingService.java
@@ -1,13 +1,13 @@
 package com.hcl.ecomm.core.services;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
+import com.hcl.ecomm.core.pojo.Ratings;
 
 import java.util.List;
 
 public interface RatingService {
 
-    public List getRatingDataSQL(String sku);
+    public List<Ratings> getRatingDataSQL(String sku);
     public JSONObject saveRating(JSONObject ratItem,String email,String sku);
 
 
