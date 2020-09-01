36ca4ae3b083fe45a8567c53aacd8f92e9ab3ee6
uditnaagar777 <udit.naagar@hcl.com>
2020-09-02 00:10:28 +0530

    Renaming component and files to lowercase

--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/SocialShare/clientlibs/js/SocialShare.js
+++
@@ -1,5 +0,0 @@
-let productName = $(".product-details-cmp__prduct-name").text();
-    let swfImageMailTo = document.getElementById('swfImageMailTo');
- 	swfImageMailTo.href = "mailto:?"
- + "&subject=" + encodeURIComponent("I want to recommend this " + productName)
- + "&body=" + encodeURIComponent("I want to recommend this product at HCL.com " + productName +"." + "\nLearn more: " + window.location.href);
