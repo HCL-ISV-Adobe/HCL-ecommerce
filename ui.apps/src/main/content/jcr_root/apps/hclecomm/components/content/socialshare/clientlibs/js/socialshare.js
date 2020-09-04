acfa95f8c0b15dd25400a497b6f320edb5c3e6ff
rajamouli <rajamoulidudekula@hcl.com>
2020-09-04 11:22:01 +0530

    consoleerrors QA fix

--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/socialshare/clientlibs/js/socialshare.js
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/socialshare/clientlibs/js/socialshare.js
@@ -1,5 +1,8 @@
 let productName = $(".product-details-cmp__prduct-name").text();
-let swfImageMailTo = document.getElementById('swfImageMailTo');
-swfImageMailTo.href = "mailto:?"
+    let swfImageMailTo = document.getElementById('swfImageMailTo');
+    let swfMailTo = document.getElementById('swfMailTo');
+	if(swfImageMailTo && swfMailTo){
+ swfMailTo.href = swfImageMailTo.href = "mailto:?"
  + "&subject=" + encodeURIComponent("I want to recommend this " + productName)
- + "&body=" + encodeURIComponent("I want to recommend this product at HCL.com " + productName +"." + "\nLearn more: " + window.location.href);
+ + "&body=" + encodeURIComponent("I want to recommend this product at HCL.com " + productName + "Learn more: " + window.location.href);
+	}
