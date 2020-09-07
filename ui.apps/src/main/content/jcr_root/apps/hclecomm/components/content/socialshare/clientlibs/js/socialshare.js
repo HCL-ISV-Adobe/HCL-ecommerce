d398b375b89833b1403c32ad707441ce14762c3b
uditnaagar777 <udit.naagar@hcl.com>
2020-09-07 14:42:35 +0530

    console error handled and email working fixed

--- ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/socialshare/clientlibs/js/socialshare.js
+++ ui.apps/src/main/content/jcr_root/apps/hclecomm/components/content/socialshare/clientlibs/js/socialshare.js
@@ -1,8 +1,7 @@
 let productName = $(".product-details-cmp__prduct-name").text();
-    let swfImageMailTo = document.getElementById('swfImageMailTo');
-    let swfMailTo = document.getElementById('swfMailTo');
-	if(swfImageMailTo && swfMailTo){
- swfMailTo.href = swfImageMailTo.href = "mailto:?"
+let swfImageMailTo = document.getElementById('swfImageMailTo');
+if(swfImageMailTo){
+swfImageMailTo.href = "mailto:?"
  + "&subject=" + encodeURIComponent("I want to recommend this " + productName)
- + "&body=" + encodeURIComponent("I want to recommend this product at HCL.com " + productName + "Learn more: " + window.location.href);
-	}
+ + "&body=" + encodeURIComponent("I want to recommend this product at HCL.com " + productName +"." + "\nLearn more: " + window.location.href);
+}
